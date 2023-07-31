e233c663bccc9da2185689e147df6f77fe190c60
Dumitru Tronciu <dumitru.tronciu@gmail.com>
2023-07-31 23:07:31 +0100

    Initial vertion of the script
    Script fulfills most of the requirements except running as a scheduled task

---
+++ assignment_2/script.py
@@ -0,0 +1,47 @@
+import shutil
+import traceback
+import json
+import re
+from datetime import datetime 
+import os
+
+def logStatus(msg):
+    # open log file and append 
+    with open(data['log']['location'], 'a') as log:
+        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S\t')
+        log.write(time +  msg + "\r\n")
+
+# parse config file
+with open("settings.json", 'r') as jsonfile:
+    data = json.load(jsonfile)
+
+target_dir = data['destination']
+
+# set destination folder
+dest_fd = target_dir + datetime.now().strftime('Backup %Y-%m-%d %H:%M:%S')
+
+
+try:
+    # copy files
+    for dir in data['source']:
+        shutil.copytree(dir, dest_fd + "/" + os.path.basename(dir))
+
+    # get all backup directories
+    with os.scandir(target_dir) as it:
+        targest = [file.name for file in it if file.is_dir() and "Backup" in file.name ]
+    
+    # delete older backups
+    targest.sort()
+    for dir in targest[data['log']['max-backups']: ]:
+        shutil.rmtree(target_dir + dir)
+
+    # log success
+    logStatus("Operation succeeded")
+except Exception as e:
+    # log any thrown exceptions
+    err = "".join(traceback.format_exception_only(e))   # use traceback to format exception message
+    err = re.sub("\[Errno \d\]", "", err)
+    msg = "Operation interrupted due to unexpected error.\n\t" + err
+    logStatus(msg)
+        
+
